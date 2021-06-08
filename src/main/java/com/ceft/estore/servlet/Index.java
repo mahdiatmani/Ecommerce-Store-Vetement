86341779c3ac715717cb0910a0384829c6d541bd
mahdi atmani <mahdiaatmani@gmail.ciom>
2021-06-08 16:19:32 +0100

    start projet hello world

---
+++ src/main/java/com/ceft/estore/servlet/Index.java
@@ -0,0 +1,21 @@
+package com.ceft.estore.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class Index extends HttpServlet {
+	
+	
+   @Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	   this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
+   
+   }
+}
