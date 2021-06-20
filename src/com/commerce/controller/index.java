3e50c4bb1ef1f961580ce670818426a26331bc93
mahdi atmani <mahdiaatmani@gmail.ciom>
2021-06-20 17:59:06 +0100

    first commit

--- src/com/commerce/controller/index.java
+++ src/com/commerce/controller/index.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class index extends HttpServlet {
-    @Override
+    @Override    
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
 
